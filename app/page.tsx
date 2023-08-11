023be60027db2e1aaeeeafc7ed029fbce5df0639
Muhammad Arif <66298095+arfakl99@users.noreply.github.com>
2023-08-11 14:15:33 +0500

    set project

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+        Hello
       </h1>
     </main>
   )
